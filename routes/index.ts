42267a156ab1398891a04ffdf724aed13b4297f9
hasansultan92 <hasansultan92@hotmail.com>
2023-03-04 20:44:07 -0500

    Refactoring in progress

--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,8 @@
-import {get_license_score} from './license_score_calc/license';
-import {get_urls, URL_PARSE} from './url_parser';
-import {create_logger} from './logging_setup';
-import {get_bus_factor_score} from './bus_factor/bus_factor';
-import {get_responsiveness_score} from './responsiveness_factor/responsiveness';
+import {get_license_score} from './MetricCalculator/license_score_calc/license';
+import {get_urls, URL_PARSE} from './MetricCalculator/url_parser';
+import {create_logger} from './MetricCalculator/logging_setup';
+import {get_bus_factor_score} from './MetricCalculator/bus_factor/bus_factor';
+import {get_responsiveness_score} from './MetricCalculator/responsiveness_factor/responsiveness';
 
 const arrayToNdjson = require('array-to-ndjson');
 

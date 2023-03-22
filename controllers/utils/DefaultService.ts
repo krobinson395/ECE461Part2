dd36fac02fd4e511e8eea032bfaf79b780049de0
hasansultan92 <hasansultan92@hotmail.com>
2023-03-21 22:23:11 -0400

    backend based on spec provided

---
+++ controllers/utils/DefaultService.ts
@@ -0,0 +1,297 @@
+/* eslint-disable no-unused-vars */
+const ServiceClass = require('./Service');
+
+/**
+ * Create an access token.
+ *
+ * authenticationRequest AuthenticationRequest
+ * returns String
+ * */
+export const createAuthToken = ({authenticationRequest}) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      resolve(
+        ServiceClass.successResponse({
+          authenticationRequest,
+        })
+      );
+    } catch (e) {
+      reject(
+        ServiceClass.rejectResponse(
+          e.message || 'Invalid input',
+          e.status || 405
+        )
+      );
+    }
+  });
+/**
+ * Delete all versions of this package.
+ *
+ * name String
+ * xAuthorization String  (optional)
+ * no response value expected for this operation
+ * */
+export const packageByNameDelete = ({name, xAuthorization}) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      resolve(
+        ServiceClass.successResponse({
+          name,
+          xAuthorization,
+        })
+      );
+    } catch (e) {
+      reject(
+        ServiceClass.rejectResponse(
+          e.message || 'Invalid input',
+          e.status || 405
+        )
+      );
+    }
+  });
+/**
+ * Return the history of this package (all versions).
+ *
+ * name String
+ * xAuthorization String  (optional)
+ * returns List
+ * */
+export const packageByNameGet = ({name, xAuthorization}) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      resolve(
+        ServiceClass.successResponse({
+          name,
+          xAuthorization,
+        })
+      );
+    } catch (e) {
+      reject(
+        ServiceClass.rejectResponse(
+          e.message || 'Invalid input',
+          e.status || 405
+        )
+      );
+    }
+  });
+/**
+ * Get any packages fitting the regular expression.
+ * Search for a package using regular expression over package names and READMEs. This is similar to search by name.
+ *
+ * regex String
+ * body String
+ * xAuthorization String  (optional)
+ * returns List
+ * */
+export const packageByRegExGet = ({regex, body, xAuthorization}) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      resolve(
+        ServiceClass.successResponse({
+          regex,
+          body,
+          xAuthorization,
+        })
+      );
+    } catch (e) {
+      reject(
+        ServiceClass.rejectResponse(
+          e.message || 'Invalid input',
+          e.status || 405
+        )
+      );
+    }
+  });
+/**
+ *
+ * xAuthorization String
+ * packageData PackageData
+ * returns Package
+ * */
+export const packageCreate = ({xAuthorization, packageData}) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      resolve(
+        ServiceClass.successResponse({
+          xAuthorization,
+          packageData,
+        })
+      );
+    } catch (e) {
+      reject(
+        ServiceClass.rejectResponse(
+          e.message || 'Invalid input',
+          e.status || 405
+        )
+      );
+    }
+  });
+/**
+ * Delete this version of the package.
+ *
+ * id String Package ID
+ * xAuthorization String  (optional)
+ * no response value expected for this operation
+ * */
+export const packageDelete = ({id, xAuthorization}) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      resolve(
+        ServiceClass.successResponse({
+          id,
+          xAuthorization,
+        })
+      );
+    } catch (e) {
+      reject(
+        ServiceClass.rejectResponse(
+          e.message || 'Invalid input',
+          e.status || 405
+        )
+      );
+    }
+  });
+/**
+ *
+ * id String
+ * xAuthorization String  (optional)
+ * returns PackageRating
+ * */
+export const packageRate = ({id, xAuthorization}) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      resolve(
+        ServiceClass.successResponse({
+          id,
+          xAuthorization,
+        })
+      );
+    } catch (e) {
+      reject(
+        ServiceClass.rejectResponse(
+          e.message || 'Invalid input',
+          e.status || 405
+        )
+      );
+    }
+  });
+/**
+ * Interact with the package with this ID
+ * Return this package.
+ *
+ * id String ID of package to fetch
+ * xAuthorization String  (optional)
+ * returns Package
+ * */
+export const packageRetrieve = ({id, xAuthorization}) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      resolve(
+        ServiceClass.successResponse({
+          id,
+          xAuthorization,
+        })
+      );
+    } catch (e) {
+      reject(
+        ServiceClass.rejectResponse(
+          e.message || 'Invalid input',
+          e.status || 405
+        )
+      );
+    }
+  });
+/**
+ * Update this content of the package.
+ * The name, version, and ID must match.  The package contents (from PackageData) will replace the previous contents.
+ *
+ * id String
+ * package Package
+ * xAuthorization String  (optional)
+ * no response value expected for this operation
+ * */
+export const packageUpdate = ({id, packageInfo, xAuthorization}) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      resolve(
+        ServiceClass.successResponse({
+          id,
+          packageInfo,
+          xAuthorization,
+        })
+      );
+    } catch (e) {
+      reject(
+        ServiceClass.rejectResponse(
+          e.message || 'Invalid input',
+          e.status || 405
+        )
+      );
+    }
+  });
+/**
+ * Get the packages from the registry.
+ * Get any packages fitting the query. Search for packages satisfying the indicated query.  If you want to enumerate all packages, provide an array with a single PackageQuery whose name is \"*\".  The response is paginated; the response header includes the offset to use in the next query.
+ *
+ * packageQuery List
+ * xAuthorization String  (optional)
+ * offset String Provide this for pagination. If not provided, returns the first page of results. (optional)
+ * returns List
+ * */
+export const packagesList = ({packageQuery, xAuthorization, offset}) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      resolve(
+        ServiceClass.successResponse({
+          packageQuery,
+          xAuthorization,
+          offset,
+        })
+      );
+    } catch (e) {
+      reject(
+        ServiceClass.rejectResponse(
+          e.message || 'Invalid input',
+          e.status || 405
+        )
+      );
+    }
+  });
+/**
+ * Reset the registry
+ * Reset the registry to a system default state.
+ *
+ * xAuthorization String  (optional)
+ * no response value expected for this operation
+ * */
+export const registryReset = ({xAuthorization}) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      resolve(
+        ServiceClass.successResponse({
+          xAuthorization,
+        })
+      );
+    } catch (e) {
+      reject(
+        ServiceClass.rejectResponse(
+          e.message || 'Invalid input',
+          e.status || 405
+        )
+      );
+    }
+  });
+
+/* module.exports = {
+  createAuthToken,
+  packageByNameDelete,
+  packageByNameGet,
+  packageByRegExGet,
+  packageCreate,
+  packageDelete,
+  packageRate,
+  packageRetrieve,
+  packageUpdate,
+  packagesList,
+  registryReset,
+}; */
--- routes/server.ts
+++ routes/server.ts
@@ -4,19 +4,28 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 4000; // Set port
+import {log} from '../controllers/utils/logger';
 
+try {
+  // Backend configuration
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(bodyParser.json());
+  app.use(cors());
 
-// Backend configuration
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.use(cors());
+  //Initialize the route endpoint and modules being passed
+  const router = require('./API/openapiRouter')(express);
+  console.log(router);
+  app.use('/api', router);
 
-
-//Initialize the route endpoint and modules being passed
-const router = require("./API/apiRouter")(express)
-app.use('/api', router);
-
-// Run the backend (magic)
-const server = app.listen(port, (req: any, res: any) => {
-  console.log(`Server is active on Port : ${port}`);
-});
+  // Run the backend (magic)
+  const server = app.listen(port, (req: any, res: any) => {
+    console.log(`Server is active on Port : ${port}`);
+  });
+} catch (error: any) {
+  console.log(error);
+  log(
+    'Something went wrong in the server.js file',
+    error,
+    parseInt(process.env.LOG_LEVEL!)
+  );
+}

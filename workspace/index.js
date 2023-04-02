57e558f9aae9e4095f3520c7ef4a24db97b5f593
Kyle <robin489@purdue.edu>
2023-04-02 15:27:19 -0400

    Updated index.js

--- workspace/index.js
+++ workspace/index.js
@@ -1,14 +1,11 @@
-
 const express = require('express');
-
 const app = express();
 
 app.use(express.static('public'));
-
 app.listen(parseInt(process.env.PORT), () => {
-	console.log('listening on 8080');
+        console.log('listening on a port?');
 });
 
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
+app.get('/', (req,res) => {
+        res.sendFile(__dirname + '/index.html');
 });

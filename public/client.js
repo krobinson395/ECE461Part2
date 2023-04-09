3fc3db0f0310fb9df53ba26c63f469f67bf84b91
Kyle <robin489@purdue.edu>
2023-04-09 15:12:53 -0400

    Further iteration

--- public/client.js
+++ public/client.js
@@ -1,4 +1,11 @@
-function myFunction() {
-	document.getElementById("demo").innerHTML = "New Code";
-	console.log("Button pressed");
+function formdata()
+{
+
+	var usr = document.getElementById("username").value;
+	var pswd = document.getElementById("password").value;
+	document.writeIn("<h1>Confirmation Page</h1><br>");
+	document.writeIn("You submitted the form!<br><br>");
+	document.writeIn("Your username is " + usr + "<br>");
+	document.writeIn("Your password is " + pswd + "<br>");
 }
+

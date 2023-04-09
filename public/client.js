b371f8262213a6fe754a127cf89950b0fd73a877
Kyle <robin489@purdue.edu>
2023-04-09 19:28:03 -0400

    fixed bug

--- public/client.js
+++ public/client.js
@@ -5,7 +5,7 @@ document.getElementById("login-form").addEventListener("submit", function(event)
 	var header = document.getElementById("welcome-header");
 	if(header) {
 		console.log(event.submitter.name);
-		if(event.submitter.name === "register-submit")
+		if(event.submitter.name === "register-button")
 		{
 			header.textContent = "Welcome New User to Ver4!"
 		}
@@ -19,7 +19,7 @@ document.getElementById("login-form").addEventListener("submit", function(event)
 		header.setAttribute("id", "welcome-header");
 		var text;
 		console.log(event.submitter.name);
-		if(event.submitter.name === "register-submit")
+		if(event.submitter.name === "register-button")
 		{
 			text = document.createTextNode("Welcome New User to Ver4!");
 		}

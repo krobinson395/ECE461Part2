aa2c296a7a48813a50d3465bdd53fa50aa1eaf90
Kyle <robin489@purdue.edu>
2023-04-09 17:05:28 -0400

    Added some safeties around the text boxes

--- public/client.js
+++ public/client.js
@@ -2,10 +2,19 @@ document.getElementById("login-form").addEventListener("submit", function(event)
 	event.preventDefault();
 
 	var username = document.getElementById("username").value;
-	var header = document.createElement("h1");
-	var text = document.createTextNode("Welcome " + username);
-	header.appendChild(text);
-	document.getElementById("welcome-message").appendChild(header);
+	var header = document.getElementById("welcome-header");
+	if(header) {
+		header.textContent = "Welcome " + username;
+	}
+	else {
+		header = document.createElement("h1");
+		header.setAttribute("id", "welcome-header");
+		var text = document.createTextNode("Welcome " + username);
+		header.appendChild(text);
+		document.getElementById("welcome-message").appendChild(header);
+	}
+	document.getElementById("username").value = "";
+	document.getElementById("password").value = "":
 });
 
 document.getElementById("ingestion-form").addEventListener("submit", function(event) {
@@ -16,4 +25,5 @@ document.getElementById("ingestion-form").addEventListener("submit", function(ev
 	var text = document.createTextNode("ingesting URL");
 	message.appendChild(text);
 	document.getElementById("ingestion-form").appendChild(message);
+	document.getElementById("ingestion-url").value ="";
 });

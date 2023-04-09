78ef376df019bf0cad1267b8f605618f0c0a2f9c
Kyle <robin489@purdue.edu>
2023-04-09 15:20:26 -0400

    further testing

--- public/client.js
+++ public/client.js
@@ -3,6 +3,8 @@ function formdata()
 
 	var usr = document.getElementById("username").value;
 	var pswd = document.getElementById("password").value;
+	console.log("Username: " + usr);
+	console.log("Passowrd: " + pswd);
 	document.writeIn("<h1>Confirmation Page</h1><br>");
 	document.writeIn("You submitted the form!<br><br>");
 	document.writeIn("Your username is " + usr + "<br>");
